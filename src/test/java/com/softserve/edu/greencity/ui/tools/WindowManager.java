27fcdb59458ee31abbefb9ab9f5c1f9ecc0a675e
TarasMalynovskyy <tarasmalynovskyy@gmail.com>
2020-07-17 16:54:58 +0300

    Add method takeScreenshot ot WindowManager helper.

--- src/test/java/com/softserve/edu/greencity/ui/tools/WindowManager.java
+++ src/test/java/com/softserve/edu/greencity/ui/tools/WindowManager.java
@@ -1,7 +1,13 @@
 package com.softserve.edu.greencity.ui.tools;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 public class WindowManager {
     private WebDriver driver;
@@ -18,5 +24,16 @@ public class WindowManager {
 
     public void maximizeWindow() {
         driver.manage().window().maximize();
+    }
+
+    public static void takeScreenshot(WebDriver driver, String pathToStore) {
+        TakesScreenshot scrShot = ((TakesScreenshot) driver);
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile = new File(pathToStore);
+        try {
+            FileUtils.copyFile(SrcFile, DestFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
