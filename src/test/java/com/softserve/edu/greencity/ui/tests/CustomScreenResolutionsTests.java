22b0c7973da232d0b46738deb40b5cb72efcd5db
ilchenkoliuba <ilchenkoliuba@ukr.net>
2020-07-22 18:39:25 +0300

    Review comments resolved.

--- src/test/java/com/softserve/edu/greencity/ui/tests/CustomScreenResolutionsTests.java
+++ src/test/java/com/softserve/edu/greencity/ui/tests/CustomScreenResolutionsTests.java
@@ -12,25 +12,4 @@ import java.io.IOException;
 
 public class CustomScreenResolutionsTests extends GreenCityTestRunner{
 
-    @DataProvider
-    public Object[][] sizes() {
-        return new Object[][]{};
-    }
-
-    //GC-487
-    //Screen resolutions:
-    //1440 x 1024
-    //1024 x 760
-    //768 x 1024
-//    @BeforeMethod
-//    public void changeResolution(ITestContext context) throws IOException {
-//        String size = context.getCurrentXmlTest().getLocalParameters()
-//                .get("windowSize");
-//        driver.manage().window().setSize(size);
-//    }
-
-    @Test(dataProvider = "successRegistrationUserCreds", description = "GC-513")
-    public void registrationCheckIfMailReceived(User userLoginCredentials) {
-
-    }
 }
