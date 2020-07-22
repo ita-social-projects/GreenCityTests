83e01d15feaceec5224e813baacc874b49472d13
ilchenkoliuba <ilchenkoliuba@ukr.net>
2020-07-22 18:41:32 +0300

    Commented out items deleted.

--- src/test/java/com/softserve/edu/greencity/ui/tests/CustomScreenResolutionsTests.java
+++ src/test/java/com/softserve/edu/greencity/ui/tests/CustomScreenResolutionsTests.java
@@ -1,36 +1,7 @@
 package com.softserve.edu.greencity.ui.tests;
 
-import com.softserve.edu.greencity.ui.data.User;
-import com.softserve.edu.greencity.ui.data.UserRepository;
-import org.openqa.selenium.Dimension;
-import org.testng.ITestContext;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import java.io.IOException;
 
 public class CustomScreenResolutionsTests extends GreenCityTestRunner{
 
-    @DataProvider
-    public Object[][] sizes() {
-        return new Object[][]{};
-    }
 
-    //GC-487
-    //Screen resolutions:
-    //1440 x 1024
-    //1024 x 760
-    //768 x 1024
-//    @BeforeMethod
-//    public void changeResolution(ITestContext context) throws IOException {
-//        String size = context.getCurrentXmlTest().getLocalParameters()
-//                .get("windowSize");
-//        driver.manage().window().setSize(size);
-//    }
-
-    @Test(dataProvider = "successRegistrationUserCreds", description = "GC-513")
-    public void registrationCheckIfMailReceived(User userLoginCredentials) {
-
-    }
 }
