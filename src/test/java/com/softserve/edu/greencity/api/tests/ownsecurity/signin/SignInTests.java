0fa547302874b79a0e175e3d1cbb5ec0962ed640
TarasMalynovskyy <tarasmalynovskyy@gmail.com>
2020-07-23 18:59:52 +0300

    Create SignInUser model. Create SignInUserBuilder with director.

--- src/test/java/com/softserve/edu/greencity/api/tests/ownsecurity/signin/SignInTests.java
+++ src/test/java/com/softserve/edu/greencity/api/tests/ownsecurity/signin/SignInTests.java
@@ -1,9 +1,9 @@
 package com.softserve.edu.greencity.api.tests.ownsecurity.signin;
 
 import com.softserve.edu.greencity.api.assertions.BaseAssertions;
+import com.softserve.edu.greencity.api.builders.userbuilder.SignInUserBuilderImpl;
+import com.softserve.edu.greencity.api.builders.userbuilder.SignInUserDirector;
 import com.softserve.edu.greencity.api.client.OwnSecurityClient;
-import com.softserve.edu.greencity.ui.data.User;
-import com.softserve.edu.greencity.ui.data.UserRepository;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,17 +12,21 @@ import java.net.HttpURLConnection;
 
 public class SignInTests {
     OwnSecurityClient ownSecurityClient;
-    User user;
+    SignInUserDirector signInUserDirector;
+    SignInUserBuilderImpl signInUserBuilder;
 
     @BeforeClass
     public void beforeClass() {
         ownSecurityClient = new OwnSecurityClient();
-        user = UserRepository.get().temporary();
+        signInUserDirector = new SignInUserDirector();
+        signInUserBuilder = new SignInUserBuilderImpl();
     }
 
     @Test
     public void signInValidationTest() {
-        Response response = ownSecurityClient.signIn(user);
+        signInUserDirector.constructSignInUserValid(signInUserBuilder);
+
+        Response response = ownSecurityClient.signIn(signInUserBuilder.getResult());
 
         BaseAssertions.checkResponse(response, HttpURLConnection.HTTP_OK);
     }
