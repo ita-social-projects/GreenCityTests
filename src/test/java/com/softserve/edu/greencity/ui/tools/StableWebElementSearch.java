32d1754a2e70cd50511676e9dd2fb53f19738c29
NahrebetskyiPavel <pnagrebetskyi@gmail.com>
2020-08-20 00:46:41 +0300

    add stable web element search interface

---
+++ src/test/java/com/softserve/edu/greencity/ui/tools/StableWebElementSearch.java
@@ -0,0 +1,44 @@
+package com.softserve.edu.greencity.ui.tools;
+
+
+import com.google.common.base.Function;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * convenient way to search Web Elements
+ */
+public interface StableWebElementSearch {
+    /**
+     * set Webdriver that should to be used
+     * @return WebDriver
+     */
+    WebDriver setDriver();
+    @Description("Find element By CSS")
+    default public WebElement SearchElementByCss(By locator){
+        return explicitSearch(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    @Step
+    @Description("Find element By xpath")
+    default public WebElement SearchElementByXpath(By xPath){
+        return explicitSearch(ExpectedConditions.visibilityOfElementLocated(xPath));
+    }
+
+    @Step
+    @Description("short explicit wait visibility Of element")
+    default public <V> V explicitSearch(Function<? super WebDriver, V> condition){
+        setDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        V element = (new WebDriverWait(setDriver(),5)).until(condition);
+        setDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return element;
+    }
+}
+
