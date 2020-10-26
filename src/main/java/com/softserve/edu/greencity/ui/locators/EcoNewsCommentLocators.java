48dc6a70b70e42ff3038c72244432c7af6066b5b
ilonaGrigorak <ilonochka185@gmail.com>
2020-10-26 19:15:02 +0200

    create locators for comment

--- src/main/java/com/softserve/edu/greencity/ui/locators/EcoNewsCommentLocators.java
+++ src/main/java/com/softserve/edu/greencity/ui/locators/EcoNewsCommentLocators.java
@@ -1,2 +1,25 @@
-package com.softserve.edu.greencity.ui.locators;public enum EcoNewsCommentLocators {
+package com.softserve.edu.greencity.ui.locators;
+
+import org.openqa.selenium.By;
+
+public enum EcoNewsCommentLocators implements Locator {
+    COMMENT_AREA(By.cssSelector("textarea")),
+    COMMENT_BUTTON(By.cssSelector("button.primary-global-button")),
+    DELETE_BUTTON(By.cssSelector("button.cta-btn.delete")),
+    EDIT_BUTTON(By.cssSelector("button.cta-btn.edit")),
+    COMMENT_TEXT(By.cssSelector("p.comment-text")),
+    COMMENT_REPLY(By.cssSelector("app-add-comment")),
+    REPLY_BUTTON(By.cssSelector("button.cta-btn.reply"));
+
+    private final By path;
+
+    EcoNewsCommentLocators(By path) {
+        this.path = path;
+    }
+
+    @Override
+    public By getPath() {
+        return path;
+    }
+
 }
