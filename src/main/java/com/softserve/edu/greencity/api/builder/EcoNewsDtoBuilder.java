6fc2f46a773677f6df1f388761708cd93d914508
Богдан Дзюбайло <banderol1024@gmail.com>
2020-11-09 11:22:28 +0200

    Created classes for EcoNews API testing

---
+++ src/main/java/com/softserve/edu/greencity/api/builder/EcoNewsDtoBuilder.java
@@ -0,0 +1,49 @@
+package com.softserve.edu.greencity.api.builder;
+
+import com.softserve.edu.greencity.api.model.EcoNewsPOSTdto;
+
+public class EcoNewsDtoBuilder {
+
+    private EcoNewsPOSTdto ecoNews;
+
+    private EcoNewsDtoBuilder() {
+        ecoNews = new EcoNewsPOSTdto();
+    }
+
+    /**
+     * Starts building
+     * @return A builder with the possibility to set properties
+     */
+    public static EcoNewsDtoBuilder ecoNewsDtoWith() {
+        return new EcoNewsDtoBuilder();
+    }
+
+    public EcoNewsDtoBuilder title(String title) {
+        this.ecoNews.title = title;
+        return this;
+    }
+
+    public EcoNewsDtoBuilder text(String text) {
+        this.ecoNews.text = text;
+        return this;
+    }
+
+    public EcoNewsDtoBuilder image(String image) {
+        this.ecoNews.image = image;
+        return this;
+    }
+
+    public EcoNewsDtoBuilder source(String source) {
+        this.ecoNews.source = source;
+        return this;
+    }
+
+    public EcoNewsDtoBuilder tags(String[] tags) {
+        this.ecoNews.tags = tags;
+        return this;
+    }
+
+    public EcoNewsPOSTdto build() {
+        return ecoNews;
+    }
+}
---
+++ src/main/java/com/softserve/edu/greencity/api/model/EcoNewsPOSTdto.java
@@ -0,0 +1,18 @@
+package com.softserve.edu.greencity.api.model;
+
+public class EcoNewsPOSTdto {
+
+    public String title;
+    public String text;
+    public String image;
+    public String source;
+    public String[] tags;
+
+    public EcoNewsPOSTdto() {
+        title = "";
+        text = "";
+        image = "";
+        source = "";
+        tags = new String[0];
+    }
+}
