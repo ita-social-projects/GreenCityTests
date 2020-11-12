9c9e5c2366a72590bb03bdfa04fa67b96688cdea
ProstoLeleka <aliejua@gmail.com>
2020-11-13 00:36:37 +0200

    Refactored RegistrationTests

--- src/test/java/com/softserve/edu/greencity/ui/tests/signup/RegisterPageTests.java
+++ src/test/java/com/softserve/edu/greencity/ui/tests/signup/RegisterPageTests.java
@@ -146,13 +146,13 @@ public class RegisterPageTests extends GreenCityTestRunner implements StableWebE
         logger.info("Enter empty values into the form");
         manualRegisterComponent.registrationWrongUser(userLoginCredentials);
         softAssert.assertEquals(manualRegisterComponent.getEmailValidatorText(),
-                "Email is required",
+                "Email is required", // test fails, bug
                 "The validation message is not equal to the expected one");
         softAssert.assertEquals(manualRegisterComponent.getUserNameValidatorText(),
                 "The name must contain 6-30 characters and can contain letters(a-z), numbers(0-9) and a dot(.)",
                 "The validation message is not equal to the expected one");
         softAssert.assertEquals(manualRegisterComponent.getPasswordValidatorText(),
-                "Password is required",
+                "Password is required", // test fails, bug
                 "The validation message is not equal to the expected one");
         softAssert.assertAll();
     }
