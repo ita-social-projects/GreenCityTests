c3497ed1d93cba57f3abc9a688263835efb38c20
ProstoLeleka <aliejua@gmail.com>
2020-11-30 12:48:03 +0200

    Implemented SignUp API tests

--- src/test/java/com/softserve/edu/greencity/api/tests/signup/SignUpApiTest.java
+++ src/test/java/com/softserve/edu/greencity/api/tests/signup/SignUpApiTest.java
@@ -16,8 +16,10 @@ import org.apache.http.client.utils.URLEncodedUtils;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
@@ -73,7 +75,7 @@ public class SignUpApiTest {
                         UserRepository.get().userCredentialsForRegistration().getEmail(),
                         UserRepository.get().userCredentialsForRegistration().getUserName(),
                         UserRepository.get().userCredentialsForRegistration().getPassword(),
-                       },
+                },
         };
     }
 
@@ -90,6 +92,27 @@ public class SignUpApiTest {
                 .contentType(ContentType.JSON);
     }
 
+   private ArrayAssertion getVerifyEmailAssertion(String email, String password) {
+       String url = new GoogleMailAPI().getconfirmURL(email,password,20);
+       List<NameValuePair> params = null;
+       try {
+           params = URLEncodedUtils.parse(new URI(url), Charset.forName("UTF-8"));
+       } catch (URISyntaxException e) {
+           e.printStackTrace();
+       }
+
+       Map<String, String> map = new HashMap<String, String>();
+       for (org.apache.http.NameValuePair param : params) {
+           map.put(param.getName(),param.getValue());
+       }
+       System.out.println(map.get("token"));
+       System.out.println(map.get("user_id"));
+       OwnSecurityClient ownSecurityClient = new OwnSecurityClient("JSON");
+       Response response = ownSecurityClient.verifyEmail(map.get("token"), map.get("user_id"));
+       response.print();
+       return new ArrayAssertion(response);
+    }
+
     private ArrayAssertion getSignInAssertion(String email, String password) {
         SignInDto credentials = SignInBuilder.signInWith()
                 .email(email)
@@ -104,7 +127,7 @@ public class SignUpApiTest {
 
     @Test(dataProvider = "emptyCredentials", testName = "GC-532, GC-534, GC-535", description = "GC-532, GC-534, GC-535")
     @Description("Verify that user is not registered, when values in request are empty")
-    public void blankCredentialsTest(String email, String name,  String password, String message) {
+    public void blankCredentialsTest(String email, String name, String password, String message) {
         ArrayAssertion assertion = getCommonCredentialsAssertion(email, name, password);
         assertion.statusCode(400)
                 .bodyArrayContains("message", message);
@@ -112,7 +135,7 @@ public class SignUpApiTest {
 
     @Test(dataProvider = "invalidCredentials", testName = "GC-536, GC-537, GC-540, GC-541", description = "GC-536, GC-537, GC-540, GC-541")
     @Description("Verify that user is not registered, when credentials are invalid")
-    public void invalidCredentialsTest(String email, String name,  String password, String message) {
+    public void invalidCredentialsTest(String email, String name, String password, String message) {
         ArrayAssertion assertion = getCommonCredentialsAssertion(email, name, password);
         assertion.statusCode(400)
                 .bodyArrayContains("message", message);
@@ -120,7 +143,7 @@ public class SignUpApiTest {
 
     @Test(dataProvider = "existingEmail", testName = "GC-528", description = "GC-528")
     @Description("Verify that the user cannot register with the already registered email address")
-    public void existingEmailTest(String email, String name,  String password, String message) {
+    public void existingEmailTest(String email, String name, String password, String message) {
         ArrayAssertion assertion = getCommonCredentialsAssertion(email, name, password);
         assertion.statusCode(400)
                 .bodyArrayContains("message", message);
@@ -128,7 +151,7 @@ public class SignUpApiTest {
 
     @Test(dataProvider = "validCredentials", testName = "GC-533", description = "GC-533")
     @Description("Verify that user can not Sign in if he didn’t verify email address in email box ")
-    public void signInWithoutVerifyingEmailTest(String email, String name,  String password, String message) {
+    public void signInWithoutVerifyingEmailTest(String email, String name, String password, String message) {
         ArrayAssertion assertion = getCommonCredentialsAssertion(email, name, password);
         assertion.statusCode(201);
         assertion = getSignInAssertion(email, password);
@@ -136,8 +159,8 @@ public class SignUpApiTest {
                 .bodyValueContains("message", message);
     }
 
-    @Test(dataProvider = "SuccessfulRegistration", testName = "GC-533", description = "GC-533")
-    @Description("Verify that user can not Sign in if he didn’t verify email address in email box ")
+   /*   @Test(dataProvider = "SuccessfulRegistration", testName = "GC-531", description = "GC-531")
+    @Description("Verify that user is registered, after he enters valid values ")
     public void signUpWithValidCredentialsTest(String email, String name,  String password) {
         ArrayAssertion assertion = getCommonCredentialsAssertion(email, name, password);
         assertion.statusCode(201);
@@ -157,7 +180,28 @@ public class SignUpApiTest {
             mappedMovies.put(param.getName(),param.getValue());
         }
         System.out.println(mappedMovies.get("token"));
+    } */
+
+    @Test(dataProvider = "SuccessfulRegistration", testName = "GC-531", description = "GC-531")
+    @Description("Verify that user is registered, after he enters valid values ")
+    public void signUpWithValidCredentialsTest(String email, String name, String password) {
+        new GoogleMailAPI().clearMail(email, password);
+        ArrayAssertion assertion = getCommonCredentialsAssertion(email, name, password);
+        assertion.statusCode(201);
+
+        assertion = getVerifyEmailAssertion(email, password);
+        assertion.statusCode(200);
+    }
+      /*  URL url = null;
+        try {
+            url = new URL(new GoogleMailAPI().getconfirmURL(email, password, 20));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        String tokenAndUserId = url.getQuery();
+        assertion = getVerifyEmailAssertion(tokenAndUserId);
+        assertion.statusCode(200); */
+
+
     }
 
-
-}
