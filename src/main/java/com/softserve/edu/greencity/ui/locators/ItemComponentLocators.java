172f25a21c541f29e9667cbc826cfa5f7ca6593e
vadkostuk <vadkostuk@gmail.com>
2020-12-30 20:24:12 +0200

    changed selector in ItemComponentLocators class

--- src/main/java/com/softserve/edu/greencity/ui/locators/ItemComponentLocators.java
+++ src/main/java/com/softserve/edu/greencity/ui/locators/ItemComponentLocators.java
@@ -3,15 +3,15 @@ package com.softserve.edu.greencity.ui.locators;
 import org.openqa.selenium.By;
 
 public enum ItemComponentLocators implements Locator {
-    TAGS(By.cssSelector(".filter-tag div")),
+    TAGS(By.cssSelector(".filter-tag span")),
     TAGS_CONTAINER(By.cssSelector(".filter-tag")),
-    IMAGE(By.cssSelector(".list-image-content")),
-    TITLE(By.cssSelector(".title-list p")),
-    CONTENT(By.cssSelector(".list-text p")),
-    CONTENT_WRAP(By.cssSelector(".list-text")),
-    DATE_OF_CREATION(By.cssSelector(".user-data-text-date")),
-    AUTHOR(By.cssSelector(".user-data-added-news > p:nth-child(2)")),
-    DATE_AND_AUTHOR_CONTAINER(By.cssSelector(".user-data-added-news"));
+    IMAGE(By.cssSelector(".eco-news_list-content")),
+    TITLE(By.cssSelector(".eco-news_list-content-title p")),
+    CONTENT(By.cssSelector(".eco-news_list-content-text p")),
+    CONTENT_WRAP(By.cssSelector(".eco-news_list-content-text")),
+    DATE_OF_CREATION(By.cssSelector(".eco-news_data-text-date")),
+    AUTHOR(By.cssSelector(".eco-news_list-autors > p:nth-child(2)")),
+    DATE_AND_AUTHOR_CONTAINER(By.cssSelector(".eco-news_list-autors"));
     
     private final By path;
     ItemComponentLocators(By path) {
