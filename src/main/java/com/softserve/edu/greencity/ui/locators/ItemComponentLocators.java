574850e54e02ad962138a75afb3ad059db68afca
vadkostuk <vadkostuk@gmail.com>
2021-01-05 13:45:37 +0200

    added new locators for listview

--- src/main/java/com/softserve/edu/greencity/ui/locators/ItemComponentLocators.java
+++ src/main/java/com/softserve/edu/greencity/ui/locators/ItemComponentLocators.java
@@ -3,15 +3,24 @@ package com.softserve.edu.greencity.ui.locators;
 import org.openqa.selenium.By;
 
 public enum ItemComponentLocators implements Locator {
-    TAGS(By.cssSelector(".filter-tag span")),
+    TAGS(By.cssSelector(".filter-tag div")),
     TAGS_CONTAINER(By.cssSelector(".filter-tag")),
-    IMAGE(By.cssSelector(".eco-news_list-content")),
-    TITLE(By.cssSelector(".eco-news_list-content-title p")),
-    CONTENT(By.cssSelector(".eco-news_list-content-text p")),
-    CONTENT_WRAP(By.cssSelector(".eco-news_list-content-text")),
-    DATE_OF_CREATION(By.cssSelector(".eco-news_data-text-date")),
-    AUTHOR(By.cssSelector(".eco-news_list-autors > p:nth-child(2)")),
-    DATE_AND_AUTHOR_CONTAINER(By.cssSelector(".eco-news_list-autors"));
+    IMAGE(By.cssSelector(".list-image-content")),
+    TITLE(By.cssSelector(".title-list p")),
+    CONTENT(By.cssSelector(".list-text p")),
+    CONTENT_WRAP(By.cssSelector(".list-text")),
+    DATE_OF_CREATION(By.cssSelector(".user-data-text-date")),
+    AUTHOR(By.cssSelector(".user-data-added-news > p:nth-child(2)")),
+    DATE_AND_AUTHOR_CONTAINER(By.cssSelector(".user-data-added-news")),
+
+    TAGS_LISTVIEW(By.cssSelector(".filter-tag span")),
+    IMAGE_LISTVIEW(By.cssSelector(".eco-news_list-content")),
+    TITLE_LISTVIEW(By.cssSelector(".eco-news_list-content-title p")),
+    CONTENT_LISTVIEW(By.cssSelector(".eco-news_list-content-text p")),
+    CONTENT_WRAP_LISTVIEW(By.cssSelector(".eco-news_list-content-text")),
+    DATE_OF_CREATION_LISTVIEW(By.cssSelector(".eco-news_data-text-date")),
+    AUTHOR_LISTVIEW(By.cssSelector(".eco-news_list-autors > p:nth-child(2)")),
+    DATE_AND_AUTHOR_CONTAINER_LISTVIEW(By.cssSelector(".eco-news_list-autors"));
     
     private final By path;
     ItemComponentLocators(By path) {
