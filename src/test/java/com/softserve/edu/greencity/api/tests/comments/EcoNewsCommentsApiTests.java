957e18e46e13dc033a4dcd637c35d11993a18d7e
vadkostuk <vadkostuk@gmail.com>
2021-01-22 16:11:45 +0200

    implemented GC-1174

--- src/test/java/com/softserve/edu/greencity/api/tests/comments/EcoNewsCommentsApiTests.java
+++ src/test/java/com/softserve/edu/greencity/api/tests/comments/EcoNewsCommentsApiTests.java
@@ -23,4 +23,17 @@ public class EcoNewsCommentsApiTests extends CommentsApiTestRunner {
         editComment.statusCode(200);
     }
 
+    @Test(testName = "GC-1174", description = "GC-1174")
+    @Description("Verify that logged user can delete his own replay on the 'News' page")
+    public void loggedUserCanDeleteHisOwnReply(){
+        CommentClient commentClient = new CommentClient(ContentType.JSON, userData.accessToken);
+        Response responseComment = commentClient.postComment(ecoNewsId, new CommentDto(0, "api comment"));
+        parentCommentId = responseComment.as(CommentModel.class).id;
+        Response responseReply = commentClient.postComment(ecoNewsId,new CommentDto(parentCommentId,"commentReply"));
+        Integer replyId = responseReply.as(CommentModel.class).id;
+        Response responseDeleteReply = commentClient.deleteComment(replyId.toString());
+        BaseAssertion deleteComment = new BaseAssertion(responseDeleteReply);
+        deleteComment.statusCode(200);
+    }
+
 }
