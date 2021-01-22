bde36d1c357645594da9255462655cb9d9f4c46c
vadkostuk <vadkostuk@gmail.com>
2021-01-22 13:35:03 +0200

    implemented GC-1163 test

--- src/test/java/com/softserve/edu/greencity/api/tests/comments/EcoNewsCommentsApiTests.java
+++ src/test/java/com/softserve/edu/greencity/api/tests/comments/EcoNewsCommentsApiTests.java
@@ -23,4 +23,15 @@ public class EcoNewsCommentsApiTests extends CommentsApiTestRunner {
         editComment.statusCode(200);
     }
 
+    @Test(testName = "GC-1163",description = "1163")
+    @Description("Verify that logged user can publish reply on News Single Page")
+    public void loggedUserCanPublishReply(){
+        CommentClient commentClient = new CommentClient(ContentType.JSON, userData.accessToken);
+        Response responseComment = commentClient.postComment(ecoNewsId, new CommentDto(0, "api comment"));
+        parentCommentId = responseComment.as(CommentModel.class).id;
+        Response responseCommentReply = commentClient.postComment(ecoNewsId,new CommentDto(parentCommentId,"gc-1163_reply"));
+        BaseAssertion publishReply = new BaseAssertion(responseCommentReply);
+        publishReply.statusCode(201);
+    }
+
 }
