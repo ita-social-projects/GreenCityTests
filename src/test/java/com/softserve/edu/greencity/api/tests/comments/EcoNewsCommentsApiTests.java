ef90017a2f1253cc82dc7c3ee3510314cbc74016
AndriiTurianskyi <andriy.turyansky@gmail.com>
2021-01-23 18:03:42 +0200

    Implemented GC-1158

--- src/test/java/com/softserve/edu/greencity/api/tests/comments/EcoNewsCommentsApiTests.java
+++ src/test/java/com/softserve/edu/greencity/api/tests/comments/EcoNewsCommentsApiTests.java
@@ -2,8 +2,13 @@ package com.softserve.edu.greencity.api.tests.comments;
 
 import com.softserve.edu.greencity.api.assertions.BaseAssertion;
 import com.softserve.edu.greencity.api.clients.CommentClient;
+import com.softserve.edu.greencity.api.clients.OwnSecurityClient;
 import com.softserve.edu.greencity.api.models.comments.CommentDto;
 import com.softserve.edu.greencity.api.models.comments.CommentModel;
+import com.softserve.edu.greencity.api.models.ownsecurity.OwnSecurityModel;
+import com.softserve.edu.greencity.api.models.ownsecurity.SignInDto;
+import com.softserve.edu.greencity.data.users.User;
+import com.softserve.edu.greencity.data.users.UserRepository;
 import io.qameta.allure.Description;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -23,4 +28,20 @@ public class EcoNewsCommentsApiTests extends CommentsApiTestRunner {
         editComment.statusCode(200);
     }
 
+    @Test(testName = "GC-1158", description = "GC-1158")
+    @Description("Verify that logged user can`t delete not his own comment on the 'News' page.")
+    public void loggedUserCanDeleteNotHisOwnComment() {
+        CommentClient temporaryClient = new CommentClient(ContentType.JSON, userData.accessToken);
+        Response responsePostComment = temporaryClient.postComment(ecoNewsId, new CommentDto(0, "Comment by temporary user"));
+        parentCommentId = responsePostComment.as(CommentModel.class).id;
+        User existUser = UserRepository.get().exist();
+        OwnSecurityClient authorizationExistClient = new OwnSecurityClient(ContentType.JSON);
+        Response signIn = authorizationExistClient
+                .signIn(new SignInDto(existUser.getEmail(), existUser.getPassword()));
+        OwnSecurityModel existUserData = signIn.as(OwnSecurityModel.class);
+        CommentClient existClient = new CommentClient(ContentType.JSON, existUserData.accessToken);
+        Response responseDeleteComment = existClient.deleteComment(parentCommentId.toString());
+        BaseAssertion deleteComment = new BaseAssertion(responseDeleteComment);
+        deleteComment.statusCode(401);
+    }
 }
