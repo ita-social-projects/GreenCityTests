f0c27aaaaf1b0b297d8499ef8359613a97998341
93mike <mishakaspar@gmail.com>
2021-01-26 19:47:39 +0200

    Implemented GC-1194

--- src/test/java/com/softserve/edu/greencity/api/tests/comments/EcoNewsCommentsApiTests.java
+++ src/test/java/com/softserve/edu/greencity/api/tests/comments/EcoNewsCommentsApiTests.java
@@ -2,8 +2,13 @@ package com.softserve.edu.greencity.api.tests.comments;
 
 import com.softserve.edu.greencity.api.assertions.BaseAssertion;
 import com.softserve.edu.greencity.api.clients.CommentClient;
+import com.softserve.edu.greencity.api.clients.OwnSecurityClient;
 import com.softserve.edu.greencity.api.models.comments.CommentDto;
 import com.softserve.edu.greencity.api.models.comments.CommentModel;
+import com.softserve.edu.greencity.api.models.ownsecurity.OwnSecurityModel;
+import com.softserve.edu.greencity.api.models.ownsecurity.SignInDto;
+import com.softserve.edu.greencity.data.users.User;
+import com.softserve.edu.greencity.data.users.UserRepository;
 import io.qameta.allure.Description;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -25,7 +30,7 @@ public class EcoNewsCommentsApiTests extends CommentsApiTestRunner {
 
     @Test(testName = "GC-1155", description = "GC-1155")
     @Description("Verify that logged user can delete his own comment on the 'News' page.")
-    public void loggedUserCanDeleteHisOwnComment(){
+    public void loggedUserCanDeleteHisOwnComment() {
         CommentClient commentClient = new CommentClient(ContentType.JSON, userData.accessToken);
         Response responseComment = commentClient.postComment(ecoNewsId, new CommentDto(0, "api comment"));
         parentCommentId = responseComment.as(CommentModel.class).id;
@@ -44,5 +49,23 @@ public class EcoNewsCommentsApiTests extends CommentsApiTestRunner {
         Response responseTryToEditComment = commentClientNotLogged.updateCommentByNotLoggedUser(parentCommentId.toString(), "new%20comment");
         BaseAssertion notEditedComment = new BaseAssertion(responseTryToEditComment);
         notEditedComment.statusCode(401);
+    }
+
+    @Test(testName = "GC-1194", description = "GC-1194")
+    @Description("Verify that logged user can’t edit not his own comment on the ‘Eco news’ page")
+    public void loggedUserCanNotEditNotHisOwnComment() {
+        CommentClient commentClientTemporary = new CommentClient(ContentType.JSON, userData.accessToken);
+        Response responseComment = commentClientTemporary.postComment(ecoNewsId, new CommentDto(0, "api comment"));
+        parentCommentId = responseComment.as(CommentModel.class).id;
+
+        OwnSecurityClient authorizationClient = new OwnSecurityClient(ContentType.JSON);
+        User existUser = UserRepository.get().exist();
+        Response signedIn = authorizationClient
+                .signIn(new SignInDto(existUser.getEmail(), existUser.getPassword()));
+        OwnSecurityModel existUserData = signedIn.as(OwnSecurityModel.class);
+        CommentClient commentClientExist = new CommentClient(ContentType.JSON, existUserData.accessToken);
+        Response responseEdit = commentClientExist.updateComment(parentCommentId.toString(), "new%20comment%20api");
+        BaseAssertion deleteComment = new BaseAssertion(responseEdit);
+        deleteComment.statusCode(400);
     }
 }
