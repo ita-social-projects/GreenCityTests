0698eb08dd5578c1188656655b6ad9d6ee472a65
mariareshetovska <mresetovska@gmail.com>
2021-02-02 15:32:34 +0200

    added TipsAndTricksModel

--- src/main/java/com/softserve/edu/greencity/api/models/tipsandtricks/TipsAndTricksPOSTdto.java
+++ src/main/java/com/softserve/edu/greencity/api/models/tipsandtricks/TipsAndTricksPOSTdto.java
@@ -1,4 +1,32 @@
 package com.softserve.edu.greencity.api.models.tipsandtricks;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class TipsAndTricksPOSTdto {
+    public String title;
+    public String text;
+    public String image;
+    public String source;
+    public String[] tags;
+
+    public TipsAndTricksPOSTdto() {
+        title = "";
+        text = "";
+        image = "";
+        source = "";
+        tags = new String[0];
+    }
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        String objAsJson = null;
+        try {
+            objAsJson = mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return objAsJson;
+    }
 }
--- src/main/java/com/softserve/edu/greencity/api/models/tipsandtricks/TipsAndTricksPageModel.java
+++ src/main/java/com/softserve/edu/greencity/api/models/tipsandtricks/TipsAndTricksPageModel.java
@@ -1,4 +1,32 @@
 package com.softserve.edu.greencity.api.models.tipsandtricks;
 
+
+import java.util.Arrays;
+
 public class TipsAndTricksPageModel {
+    public TipsAndTricksModel[] page;
+    public Long totalElements;
+    public Long currentPage;
+    public Long totalPages;
+
+    public TipsAndTricksPageModel(TipsAndTricksModel[] page, long total, long current, long pages) {
+        this.page = page;
+        totalElements = total;
+        currentPage = current;
+        totalPages = pages;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof TipsAndTricksAuthorModel)) {
+            return false;
+        }
+        TipsAndTricksPageModel p = (TipsAndTricksPageModel) obj;
+
+        return Arrays.equals(page, p.page) && totalPages.equals(p.totalElements)
+                && currentPage.equals(p.currentPage) && totalPages.equals(p.totalPages);
+    }
 }
