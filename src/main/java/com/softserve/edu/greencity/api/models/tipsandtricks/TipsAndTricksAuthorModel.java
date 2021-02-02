c5ae975368dd7315aa20ab5fdf67a8bd90a44d24
Hanna Vasiunyk <annvasiunyk@gmail.com>
2021-02-02 17:37:09 +0200

    Added Author model and builder

--- src/main/java/com/softserve/edu/greencity/api/models/tipsandtricks/TipsAndTricksAuthorModel.java
+++ src/main/java/com/softserve/edu/greencity/api/models/tipsandtricks/TipsAndTricksAuthorModel.java
@@ -1,4 +1,31 @@
 package com.softserve.edu.greencity.api.models.tipsandtricks;
 
+
 public class TipsAndTricksAuthorModel {
+
+    public Long id;
+    public String name;
+
+    public TipsAndTricksAuthorModel() {
+        id = 0L;
+        name = "";
+    }
+
+    public TipsAndTricksAuthorModel(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof TipsAndTricksAuthorModel)) {
+            return false;
+        }
+        TipsAndTricksAuthorModel a = (TipsAndTricksAuthorModel) obj;
+
+        return id.equals(a.id) && name.equals(a.name);
+    }
 }
