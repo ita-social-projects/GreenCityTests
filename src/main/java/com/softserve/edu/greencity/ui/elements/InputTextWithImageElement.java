3e9c87061afb4f5c1445e25559157c219a6e01df
Kuderiavets-Nata <73226595+Kuderiavets-Nata@users.noreply.github.com>
2021-03-08 20:10:01 +0200

    New elements

--- src/main/java/com/softserve/edu/greencity/ui/elements/InputTextWithImageElement.java
+++ src/main/java/com/softserve/edu/greencity/ui/elements/InputTextWithImageElement.java
@@ -4,7 +4,7 @@ import com.softserve.edu.greencity.ui.locators.Locator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class InputTextWithImageElement extends InputTextElement{
+public class InputTextWithImageElement extends InputTextElement {
     public InputTextWithImageElement(WebDriver driver, Locator locator) {
         super(driver, locator);
     }
@@ -15,5 +15,43 @@ public class InputTextWithImageElement extends InputTextElement{
 
     public InputTextWithImageElement(WebElement element) {
         super(element);
+    }
+
+    public void click() {
+        this.element.click();
+    }
+
+    public void enterText(String text) {
+        this.element.sendKeys(text);
+    }
+
+    public String getTextFromInput() {
+        return element.getAttribute("value");
+    }
+
+    public void clearInput() {
+        this.element.clear();
+    }
+
+    public boolean isEmpty() {
+        return this.element.getAttribute("value").isEmpty();
+    }
+
+    public void changeText(String newText) {
+        this.element.click();
+        this.element.clear();
+        this.element.sendKeys(newText);
+    }
+
+    public int getImageWight() {
+        return this.element.getSize().width;
+    }
+
+    public int getImageHeight() {
+        return this.element.getSize().height;
+    }
+
+    public boolean isImageVisible() {
+        return this.element.isDisplayed();
     }
 }
