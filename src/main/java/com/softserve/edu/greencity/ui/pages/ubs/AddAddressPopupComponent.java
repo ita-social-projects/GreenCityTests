c75506ec97defaf44f1a89b200da2884cd8e5b58
AlonaLutsenko <alonalutsenkobb@gmail.com>
2021-07-13 22:43:29 +0300

    PopUp

--- src/main/java/com/softserve/edu/greencity/ui/pages/ubs/AddAddressPopupComponent.java
+++ src/main/java/com/softserve/edu/greencity/ui/pages/ubs/AddAddressPopupComponent.java
@@ -4,9 +4,10 @@ import com.softserve.edu.greencity.ui.elements.ButtonElement;
 import com.softserve.edu.greencity.ui.elements.InputElement;
 import com.softserve.edu.greencity.ui.elements.LabelElement;
 import com.softserve.edu.greencity.ui.locators.ubs.AddAddressPopupLocators;
-import com.softserve.edu.greencity.ui.locators.ubs.OrderDetailsPageLocators;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddAddressPopupComponent extends UBSCourierBasePage {
@@ -14,13 +15,18 @@ public class AddAddressPopupComponent extends UBSCourierBasePage {
 
     private ButtonElement cancelButton;
     private ButtonElement addAddressButton;
-    private InputElement cityInput;
+
+    private WebElement cityInput;
+
     private InputElement districtInput;
     private InputElement streetInput;
     private InputElement houseInput;
     private InputElement corpInput;
     private InputElement entranceInput;
+
     private LabelElement cityLabel;
+    private LabelElement cityAlertMessage;
+    private LabelElement districtAlertMessage;
 
     public AddAddressPopupComponent(WebDriver webDriver) {
         super(webDriver);
@@ -30,23 +36,63 @@ public class AddAddressPopupComponent extends UBSCourierBasePage {
     public void initElements() {
         cancelButton = new ButtonElement(driver, AddAddressPopupLocators.CANCEL_BUTTON);
         addAddressButton = new ButtonElement(driver, AddAddressPopupLocators.ADD_ADDRESS_BUTTON);
-        cityInput = new InputElement(driver, AddAddressPopupLocators.CITY_INPUT);
-        districtInput = new InputElement(driver, AddAddressPopupLocators.DISTRICT_INPUT);
-        streetInput = new InputElement(driver, AddAddressPopupLocators.STREET_INPUT);
+        //cityInput = new DropDownElement(driver, AddAddressPopupLocators.CITY_INPUT);
         houseInput = new InputElement(driver, AddAddressPopupLocators.HOUSE_INPUT);
         corpInput = new InputElement(driver, AddAddressPopupLocators.CORP_INPUT);
         entranceInput = new InputElement(driver, AddAddressPopupLocators.ENTRANCE_INPUT);
         cityLabel = new LabelElement(driver, AddAddressPopupLocators.CITY_LABEL);
+        cancelButton = new ButtonElement(driver, AddAddressPopupLocators.CANCEL_BUTTON);
+    }
+    //TODO write crear
+    public WebElement getStreetInputt() {
+        wait = new WebDriverWait(driver, 5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(getStreetInput));
+        return driver.findElement(getStreetInput);
     }
 
-    private ButtonElement getCancelButton() {
-        if (cancelButton == null) {
-            cancelButton = new ButtonElement(driver, AddAddressPopupLocators.CANCEL_BUTTON);
+    public AddAddressPopupComponent fillStreet(Street street) {
+        getStreetInput().sendKeys();
+        return this;
+    }
+
+
+    //TODO check this method
+    public LabelElement getCityAlertMessage() {
+        if (cityAlertMessage == null) {
+            cityAlertMessage = new LabelElement(driver, AddAddressPopupLocators.CITY_ALERT_MESSAGE);
         }
-        return cancelButton;
+        return cityAlertMessage;
     }
 
-    private ButtonElement getAddButton() {
+    public LabelElement getDistrictAlertMessage() {
+        if (cityAlertMessage == null) {
+            cityAlertMessage = new LabelElement(driver, AddAddressPopupLocators.DISTRICT_ALERT_MESSAGE);
+        }
+        return cityAlertMessage;
+    }
+
+    public WebElement getCityInput() {
+        if (cityInput == null) {
+            cityInput = driver.findElement(AddAddressPopupLocators.CITY_INPUT.getPath());
+        }
+        return cityInput;
+    }
+
+    public InputElement getStreetInput() {
+        if (streetInput == null) {
+            streetInput = new InputElement(driver, AddAddressPopupLocators.STREET_INPUT);
+        }
+        return streetInput;
+    }
+
+    public InputElement getDistrictInput() {
+        if (districtInput == null) {
+            districtInput = new InputElement(driver, AddAddressPopupLocators.DISTRICT_INPUT);
+        }
+        return districtInput;
+    }
+
+    public ButtonElement getAddButton() {
         if (addAddressButton == null) {
             addAddressButton = new ButtonElement(driver, AddAddressPopupLocators.ADD_ADDRESS_BUTTON);
         }
--- src/main/java/com/softserve/edu/greencity/ui/pages/ubs/UserAddress.java
+++ src/main/java/com/softserve/edu/greencity/ui/pages/ubs/UserAddress.java
@@ -1,2 +1,66 @@
-package com.softserve.edu.greencity.ui.pages.ubs;public class UserAddress {
+package com.softserve.edu.greencity.ui.pages.ubs;
+
+public class UserAddress {
+    private String street;
+    private String district;
+    private String house;
+    private String corp;
+    private String entrance;
+
+    public UserAddress(String street, String district, String house, String corp, String entrance) {
+        this.street = street;
+        this.district = district;
+        this.house = house;
+        this.corp = corp;
+        this.entrance = entrance;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    // setters
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    //getters
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getCorp() {
+        return corp;
+    }
+
+    public void setCorp(String corp) {
+        this.corp = corp;
+    }
+
+    public String getEntrance() {
+        return entrance;
+    }
+
+    public void setEntrance(String entrance) {
+        this.entrance = entrance;
+    }
+
+    @Override
+    public String toString() {
+        return "User [street=" + street + ", district=" + district
+                + ", house=" + house + ", entrance=" + entrance + "]";
+    }
 }
