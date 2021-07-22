9faafb0b666538e3ed25126a2e48ba36a5c2d28f
AlonaLutsenko <alonalutsenkobb@gmail.com>
2021-07-22 10:08:00 +0300

    additionalChanges

--- src/test/java/com/softserve/edu/greencity/ui/tests/ubscourier/AddAddressPopupTest.java
+++ src/test/java/com/softserve/edu/greencity/ui/tests/ubscourier/AddAddressPopupTest.java
@@ -56,13 +56,5 @@ public class AddAddressPopupTest extends GreenCityTestRunner {
     @Test(testName = "GC-1925", description = "GC-1925")
     @Description("GC-1925")
     public void checkStreetMessageInfo() {
-        logger.info("Verify that the window will be closed and all the data on the pop-up shall be erased, when the user clicks on the button “Відмінити” on address pop-up menu");
-        orderDetailsPage
-                .getServicesComponents()
-                .get(0)
-                .getInput()
-                .sendKeys("10");
-
-
-    }
+            }
 }
