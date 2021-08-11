4477456f653c72968edd343ef52a854cd508a378
VictoriaBadenko <Victoria.badenko.vb@gmail.com>
2021-08-11 20:55:49 +0300

    Added 1909 test

--- src/test/java/com/softserve/edu/greencity/api/tests/ubscourier/UBSPersonalDataAPITests.java
+++ src/test/java/com/softserve/edu/greencity/api/tests/ubscourier/UBSPersonalDataAPITests.java
@@ -2,6 +2,8 @@ package com.softserve.edu.greencity.api.tests.ubscourier;
 
 import com.softserve.edu.greencity.api.assertions.BaseAssertion;
 import com.softserve.edu.greencity.api.clients.UBSCourierClient;
+import com.softserve.edu.greencity.api.models.ubscourier.UBSCourierPOSTModel;
+import com.softserve.edu.greencity.data.UBS.UBSDataStrings;
 import com.softserve.edu.greencity.ui.tools.jdbc.dao.UBSPersonalDataDao;
 import com.softserve.edu.greencity.ui.tools.jdbc.services.UBSPersonalDataService;
 import io.qameta.allure.Description;
@@ -23,7 +25,7 @@ public class UBSPersonalDataAPITests extends UbsTestRunner {
         Response response = ubsClient.getUserPersonalData();
         BaseAssertion correspondsData = new BaseAssertion(response);
         correspondsData.statusCode(200)
-                       .bodyValueEquals("email", "343@dfgdf.n")
+                .bodyValueEquals("email", "343@dfgdf.n")
                 .bodyValueEquals("firstName", "dfgdfgdf")
                 .bodyValueEquals("id", "332")
                 .bodyValueEquals("lastName", "dfgdfgdfg")
@@ -35,13 +37,11 @@ public class UBSPersonalDataAPITests extends UbsTestRunner {
     @Description("Verify that the deleted data for the ubs_user in the database is null for the data in the server response.")
     public void deletedDataInDatabaseIsNull() {
         UBSPersonalDataService ubsPersonalDataDao = new UBSPersonalDataService();
-        ubsPersonalDataDao.deleteUser(312);
+        ubsPersonalDataDao.deleteUser(332);
         UBSCourierClient ubsClient = new UBSCourierClient(ContentType.JSON, userData.accessToken);
         Response response = ubsClient.getUserPersonalData();
         BaseAssertion deletedData = new BaseAssertion(response);
         deletedData.statusCode(200);
-        ubsPersonalDataService.getDataById(312);
-
     }
 
     @Test(testName = "GC-1854", description = "GC-1854")
@@ -49,8 +49,27 @@ public class UBSPersonalDataAPITests extends UbsTestRunner {
     public void dataInDatabaseCorresponds() {
         UBSCourierClient ubsClient = new UBSCourierClient(ContentType.JSON, userData.accessToken);
         Response response = ubsClient.getUserPersonalData();
-        BaseAssertion deletedData = new BaseAssertion(response);
-        deletedData.statusCode(200);
+        BaseAssertion correspondsData = new BaseAssertion(response);
+        correspondsData.statusCode(200)
+                .bodyValueEquals("email", "343@dfgdf.n")
+                .bodyValueEquals("firstName", "dfgdfgdf")
+                .bodyValueEquals("id", "332")
+                .bodyValueEquals("lastName", "dfgdfgdfg")
+                .bodyValueEquals("phoneNumber", "060606060")
+                .bodyValueEquals("addressComment", "dfgf");
     }
+
+    @Test(testName = "GC-1909", description = "GC-1909")
+    @Description("Verify that the system does not create an order if the 'first name' field contains special characters except .'-.")
+    public void firstNameContainsSpecialCharacters() {
+        UBSCourierClient ubsClient = new UBSCourierClient(ContentType.JSON, userData.accessToken);
+        Response response = ubsClient.postProcessUserOrder(dto);}
+
+        UBSCourierPOSTModel personalData = ubsCourierWith()
+                .firstName(UBSDataStrings.PERSONAL_DATA_NAME.getMessage())
+                .lastName(UBSDataStrings.PERSONAL_DATA_SURNAME.getMessage())
+                .email(UBSDataStrings.PERSONAL_DATA_EMAIL.getMessage())
+                .phoneNumber(UBSDataStrings.PERSONAL_DATA_PHONE.getMessage())
+                .build();
 
 }
